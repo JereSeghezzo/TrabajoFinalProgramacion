b397c9dd56f5a69f8cc1af3fe3e153f039864c29
JereSeghezzo <jeremiasseghezzo@gmail.com>
2022-06-20 20:40:40 -0300

    Player Overhaul

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,27 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float runSpeed;
-    [SerializeField] private float jumpForce;
+    [Header("Alien State")]
+    public string State;
 
+    private float runSpeed;
+    private float jumpForce;
+
+    [Header("Gravity Force")]
     [SerializeField] private float Gforce;
 
-    bool gravity;
+    [Header("Stats When Normal Size")]
+    [SerializeField] private float runSpeedBig;
+    [SerializeField] private float jumpForceBig;
 
-    public bool IsGrounded;
-    public bool change;
+    [Header("Stats When Small Size")]
+    [SerializeField] private float runSpeedSmall;
+    [SerializeField] private float jumpForceSmall;
+
+    [HideInInspector] public bool IsGrounded;
+    [HideInInspector] public bool change;
+
+    [Header("Ability Cool Down")]
+    [SerializeField] private float AbilityCD;
+    private float _abilityCD;
 
     private float camsize = 9;
 
     bool grow;
+    bool gravity;
 
+    [Header("GroundChecks")]
     public GameObject groundCheck1;
     public GameObject groundCheck2;
 
-
     Rigidbody2D rb;
     SpriteRenderer sprite;
+
+    [Header("Camera")]
     public Camera cam;
+
+    [Header("Alien Sprites")]
+    public Sprite GreenAlien;
+    public Sprite YellowAlien;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -32,46 +54,45 @@ public class PlayerController : MonoBehaviour
 
         Gforce = 16;
         camsize = 9;
-        runSpeed = 10f;
-        jumpForce = 5f;
+        runSpeed = runSpeedBig;
+        jumpForce = jumpForceBig;
         gravity = true;
-
+        _abilityCD = AbilityCD;
     }
+
     void Update()
     {
-        if(!grow && camsize < 9f)
-        {
-            camsize += Time.deltaTime * 20f;
-        }
-
-        if (grow && camsize > 2.5f)
-        {
-            camsize -= Time.deltaTime * 20f;
-        }
-
-       
+     if(_abilityCD < AbilityCD)
+     {
+      _abilityCD += Time.deltaTime;
+     }    
     
+     Movement();
 
-        cam.orthographicSize = camsize;
+     CameraSizeAdjust();
 
-        if (Input.GetKeyDown("w") && change)
+     if (Input.GetKeyDown("w") && IsGrounded == true)
         {
-            gravity = !gravity;
-            change = false;
+          CharacterChange();
         }
 
-        if (Input.GetKeyDown("f") && !grow)
+     if (Input.GetKeyDown("f") && _abilityCD >= AbilityCD)
         {
-           transform.localScale -= new Vector3(0.8f, 0.8f, 0.8f);
-            grow = !grow;
-        }
+          if(State == "Green")
+          {
+            GravityChange();
+          }else if(State == "Yellow")
+          {
+            SizeChange();
+          }
 
-        if (Input.GetKeyDown("g") && grow)
-        {
-            transform.localScale += new Vector3(0.8f, 0.8f, 0.8f);
-            grow = !grow;
-        }
-        if (Input.GetKey("d"))
+          _abilityCD = 0f;
+        } 
+    }
+
+    public void Movement()
+    {
+      if (Input.GetKey("d"))
         {
             rb.velocity = new Vector2(runSpeed, rb.velocity.y);
         }
@@ -84,8 +105,18 @@ public class PlayerController : MonoBehaviour
         {
             rb.velocity = new Vector2(0, rb.velocity.y);
         }
+        if (Input.GetKeyDown("space") && gravity == true && IsGrounded == true)
+        {
+            rb.AddForce(transform.up * jumpForce * 100);
+        
+        }
 
-        if(gravity)
+        if (Input.GetKeyDown("space") && gravity == false && IsGrounded == true)
+        {
+            rb.AddForce(transform.up * -jumpForce * 100);
+
+        }    
+         if(gravity)
         {
             rb.AddForce(transform.up * -Gforce);
             sprite.flipY = false;
@@ -102,19 +133,69 @@ public class PlayerController : MonoBehaviour
             groundCheck1.SetActive(false);
             groundCheck2.SetActive(true);
         }
-
-        if (Input.GetKeyDown("space") && gravity == true && IsGrounded == true)
-        {
-            rb.AddForce(transform.up * jumpForce * 100);
-        
-        }
-
-        if (Input.GetKeyDown("space") && gravity == false && IsGrounded == true)
-        {
-            rb.AddForce(transform.up * -jumpForce * 100);
-
-        }
     }
 
- 
+   void GravityChange()
+   {
+     if (change)
+        {
+        gravity = !gravity;
+        change = false;
+        }
+   }
+
+   void SizeChange()
+   {
+    if (!grow)
+        {
+        transform.localScale -= new Vector3(0.8f, 0.8f, 0.8f);
+        grow = !grow;
+        runSpeed = runSpeedSmall;
+        jumpForce = jumpForceSmall;
+        } else  if (grow)
+        {
+        transform.localScale += new Vector3(0.8f, 0.8f, 0.8f);
+        grow = !grow;
+        runSpeed = runSpeedBig;
+        jumpForce = jumpForceBig;
+        }
+   }
+
+   void CameraSizeAdjust()
+   {
+    cam.orthographicSize = camsize;
+    if(!grow && camsize < 9f)
+        {
+            camsize += Time.deltaTime * 20f;
+        }
+
+        if (grow && camsize > 2.5f)
+        {
+            camsize -= Time.deltaTime * 20f;
+        }
+   }
+
+   void CharacterChange()
+   {
+    if(State == "Green")
+        {
+            State = "Yellow";
+            sprite.sprite = YellowAlien;
+        } else if(State == "Yellow")
+        {
+            State = "Green";
+            sprite.sprite = GreenAlien;
+        }
+
+     gravity = true;
+
+     if (grow)
+        {
+        transform.localScale += new Vector3(0.8f, 0.8f, 0.8f);
+        grow = !grow;
+        }
+
+     runSpeed = runSpeedBig;
+     jumpForce = jumpForceBig;
+   }
 }
